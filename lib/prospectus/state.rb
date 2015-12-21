6631ba40ee2a93b0a7a8e7205e7d2272dca2bb7c
Les Aker <me@lesaker.org>
2015-12-21 18:01:15 -0500

    prototype extension

--- lib/prospectus/state.rb
+++ lib/prospectus/state.rb
@@ -16,6 +16,14 @@ module Prospectus
   class StateDSL
     def initialize(state)
       @state = state
+      @extended = false
+    end
+
+    def method_missing(method, *args, &block)
+      return super if @extended
+      module_obj = Prospectus.modules[method]
+      return super unless module_obj
+      extend module_obj
     end
   end
 end
