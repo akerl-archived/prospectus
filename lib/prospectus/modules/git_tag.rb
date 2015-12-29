4344fae0b1eccc7e250b0ecaf05bc101cf2c110c
Les Aker <me@lesaker.org>
2015-12-29 01:45:22 -0500

    handle tag errors

--- lib/prospectus/modules/git_tag.rb
+++ lib/prospectus/modules/git_tag.rb
@@ -6,7 +6,8 @@ module LogCabin
       include Prospectus.helpers.find(:regex)
 
       def load!
-        tag = `git describe --tags --abbrev=0`.chomp
+        tag = `git describe --tags --abbrev=0 2>/dev/null`.chomp
+        fail('No tags found') if tag.empty?
         @state.value = regex_helper(tag)
       end
     end
