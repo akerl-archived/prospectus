cea5e6f27b425028a091adc3365d401b77985cf3
Les Aker <me@lesaker.org>
2019-01-03 01:37:59 -0500

    fix up framework

--- cmd/check.go
+++ cmd/check.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/akerl/prospectus-ng/prospectus"
 
@@ -9,14 +11,64 @@ import (
 )
 
 func checkRunner(cmd *cobra.Command, args []string) error {
+	flags := cmd.Flags()
+	flagAll, err := flags.GetBool("all")
+	if err != nil {
+		return err
+	}
+	flagJSON, err := flags.GetBool("json")
+	if err != nil {
+		return err
+	}
+
 	if len(args) == 0 {
 		args = []string{"."}
 	}
-	
-	results = prospectus.Results{}
-	for _, x := args {
-		// TODO: make this run in parallel
+
+	checks := prospectus.Checks{}
+	err = checks.Populate(args)
+	if err != nil {
+		return err
 	}
+	results, err := checks.Execute()
+	if err != nil {
+		return err
+	}
+
+	var show prospectus.Results
+	if flagAll {
+		show = results
+	} else {
+		for _, i := range results {
+			if i.Matches() {
+				show = append(show, i)
+			}
+		}
+	}
+
+	var output string
+	if flagJSON {
+		data, err := json.MarshalIndent(show, "", "  ")
+		if err != nil {
+			return err
+		}
+		output = string(data)
+	} else {
+		var b strings.Builder
+		for _, i := range show {
+			_, err := b.WriteString(i.String())
+			if err != nil {
+				return err
+			}
+			_, err = b.WriteString("\n")
+			if err != nil {
+				return err
+			}
+		}
+		output = b.String()
+	}
+	fmt.Println(output)
+	return nil
 }
 
 var checkCmd = &cobra.Command{
