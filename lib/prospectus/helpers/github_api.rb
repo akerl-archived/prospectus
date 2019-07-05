7f97c419b7c15f56921b28b4072830f6d801bfe1
Les Aker <me@lesaker.org>
2019-07-05 21:37:25 +0000

    support optional endpoint for github_api

--- lib/prospectus/helpers/github_api.rb
+++ lib/prospectus/helpers/github_api.rb
@@ -5,21 +5,39 @@ module LogCabin
     ##
     # Provide an api method for modules to query GitHub
     module GithubApi
-      def github_api
-        @github_api ||= Octokit::Client.new(
-          access_token: auth.token,
-          auto_paginate: true
-        )
+      def github_api(endpoint = nil)
+        cached_clients[endpoint] ||= Octokit::Client.new(octokit_args(endpoint))
       end
 
       private
 
-      def auth
-        @auth ||= Octoauth.new(
+      def octokit_args(endpoint)
+        args = {
+          access_token: auth(endpoint).token,
+          auto_paginate: true
+        }
+        args[:api_endpoint] = endpoint if endpoint
+        args
+      end
+
+      private
+
+      def cached_clients
+        @cached_clients ||= {}
+      end
+
+      def auth(endpoint)
+        @auth ||= Octoauth.new(octoauth_args)
+      end
+
+      def octoauth_args(endpoint)
+        args = {
           note: 'Prospectus',
           file: :default,
           autosave: true
-        )
+        }
+        args[:api_endpoint] = endpoint if endpoint
+        args
       end
 
       def repo(value)
