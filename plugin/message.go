0b8796564d9ed4543792cd9e7d5b0bd9ae36301c
Les Aker <me@lesaker.org>
2019-11-22 15:24:46 +0000

    fix error format

--- plugin/message.go
+++ plugin/message.go
@@ -35,7 +35,7 @@ func ReadMessage(input []byte, output interface{}) error {
 		return err
 	}
 	if m.Version != apiVersion {
-		return fmt.Errorf("Plugin version mismatch: %s (expected) vs %s (actual)", apiVersion, m.Version)
+		return fmt.Errorf("Plugin version mismatch: %d (expected) vs %d (actual)", apiVersion, m.Version)
 	}
 	return json.Unmarshal(m.Contents, output)
 }
